c3a39fca23da7ec3e2c0ef6835334512b33c8aa1
MARTINS2309 <martins2309@gmail.com>
2021-10-22 14:41:50 +0100

    Babel integration to transpile ES6 to ES5 for Jest

---
+++ JavaScript30/post-tdd/14 - JavaScript References VS Copying/14.test.js
@@ -0,0 +1,30 @@
+
+import arrayOfPlayers from './14';
+
+describe('players array', () => {
+    it('Is not null', () => {
+        expect(arrayOfPlayers[0]).not.toBeNull();
+    });
+
+    it('Has the original elements and is not changed', () => {
+        expect(arrayOfPlayers[0][3]).toBe('Poppy');
+    });
+    
+    it('Has 4 elements in the array', () => {
+        expect(arrayOfPlayers[0]).toHaveLength(4);
+    });
+});
+
+describe('players2 array', () => {
+    it('Is not null', () => {
+        expect(arrayOfPlayers[1]).not.toBeNull();
+    });
+
+    it('Has the changed values', () => {
+        expect(arrayOfPlayers[1][3]).toBe('Donald');
+    });
+    
+    it('Has 4 elements in the array', () => {
+        expect(arrayOfPlayers[1]).toHaveLength(4);
+    });
+});
